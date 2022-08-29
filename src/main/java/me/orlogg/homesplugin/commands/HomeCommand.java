1359a5687c7065ddbd07a6fbe301e268de6ca684
Daniel Miranda Barcelona <73300841+0rlogg@users.noreply.github.com>
2022-08-29 02:08:16 +0200

    Update

--- src/main/java/me/orlogg/homesplugin/commands/HomeCommand.java
+++ src/main/java/me/orlogg/homesplugin/commands/HomeCommand.java
@@ -1,14 +1,12 @@
 package me.orlogg.homesplugin.commands;
 
 import me.orlogg.homesplugin.HomesPlugin;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.FileConfigurationOptions;
 import org.bukkit.entity.Player;
-
-import java.io.File;
 
 public class HomeCommand implements CommandExecutor {
     private final HomesPlugin plugin;
@@ -20,16 +18,13 @@ public class HomeCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (sender instanceof Player player) {
-
-            File file = new File("plugins/HomesPlugin", "configuration.yml");
-
-            YamlConfiguration fm = YamlConfiguration.loadConfiguration(file);
-
-            int test = fm.getInt("name");
+            FileConfiguration Home = plugin.getConfig();
+            Home.getLocation("config.yml");
+            player.sendMessage(String.valueOf(Home));
 
         }else {
 
-            System.out.println("uwu");
+             System.out.println("uwu");
         }
         return true;
     }
