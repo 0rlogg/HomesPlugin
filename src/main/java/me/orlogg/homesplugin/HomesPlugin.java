adbeaaf316e543b5476903413d1dfd5ab431d3fa
Daniel Miranda Barcelona <73300841+0rlogg@users.noreply.github.com>
2022-08-31 01:21:53 +0200

    update

--- src/main/java/me/orlogg/homesplugin/HomesPlugin.java
+++ src/main/java/me/orlogg/homesplugin/HomesPlugin.java
@@ -1,24 +1,30 @@
 package me.orlogg.homesplugin;
 
-import me.orlogg.homesplugin.commands.HomeCommand;
+import me.orlogg.homesplugin.commands.Home;
 import me.orlogg.homesplugin.commands.SetHomeCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
-public final class HomesPlugin extends JavaPlugin {
+
+public class HomesPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
-        //Allows to use a .yml file to save the coords.
-        getConfig().options().copyDefaults();
-        saveConfig();
-
-        Objects.requireNonNull(getCommand("sethome")).setExecutor(new SetHomeCommand(this));
-        Objects.requireNonNull(getCommand("home")).setExecutor(new HomeCommand(this));
-
-
+        loadConfig();
+        new SetHomeCommand(this);
+        new Home(this);
+        Objects.requireNonNull(getCommand("SetHome")).setExecutor(new SetHomeCommand(this));
+        Objects.requireNonNull(getCommand("Home")).setExecutor(new Home(this));
 
     }
+
+    private void loadConfig(){
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
 }
+
+
+
+
--- src/main/java/me/orlogg/homesplugin/commands/SetHomeCommand.java
+++ src/main/java/me/orlogg/homesplugin/commands/SetHomeCommand.java
@@ -1,34 +1,57 @@
 package me.orlogg.homesplugin.commands;
 
+
 import me.orlogg.homesplugin.HomesPlugin;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import javax.annotation.Nonnull;
+import java.util.Objects;
 
-public class SetHomeCommand implements CommandExecutor {
+public class SetHomeCommand implements CommandExecutor{
 
-    private final HomesPlugin plugin;
+    private final HomesPlugin Plugin;
 
-    public SetHomeCommand(HomesPlugin plugin) {
-        this.plugin = plugin;
+    public SetHomeCommand (HomesPlugin Plugin){
+        this.Plugin = Plugin;
+
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@Nonnull CommandSender sender,@Nonnull  Command command,@Nonnull  String label,@Nonnull  String[] args) {
 
-        if (sender instanceof Player player) {
+        if (!(sender instanceof Player player)){
+            sender.sendMessage("Player only command!");
+            return false;
 
-            Location location = player.getLocation();
-            plugin.getConfig().set("Home", location);
-            plugin.saveConfig();
-            player.sendMessage("Home location set!");
-
-        }else {
-
-            System.out.println("uwu");
         }
+        if (!player.hasPermission("HomesPlugin.SetHomePlugin")){
+            player.sendMessage("no tienes permisos");
+        }
+        if(args.length == 0){
+            player.sendMessage("You need to give a name");
+            return false;
+        }
+        String name = args[0].toLowerCase();
+        if (Plugin.getConfig().get(name) != null){
+            player.sendMessage("Ya hay un home con ese nombre");
+            return false;
+
+        }
+
+        Location location = player.getLocation();
+        Plugin.getConfig().set(name + ".World", Objects.requireNonNull(location.getWorld()).getName());
+        Plugin.getConfig().set(name + ".X", location.getX());
+        Plugin.getConfig().set(name + ".Y", location.getY());
+        Plugin.getConfig().set(name + ".Z", location.getZ());
+        Plugin.getConfig().set(name + ".Pitch", location.getPitch());
+        Plugin.getConfig().set(name + ".Yaw", location.getYaw());
+        Plugin.saveConfig();
+        player.sendMessage("Home set");
+
         return true;
     }
 }
+
